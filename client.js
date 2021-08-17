ad73b496e800777918d16a83934afab2300ade5d
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-17 15:05:53 -0400

    implemented setTimeOut

--- client.js
+++ client.js
@@ -1,8 +1,13 @@
 
-const { Server } = require('http');
+//const { Server } = require('http');
 const net = require('net');
 
 const connect = function () {
+  const name = 'Name: SCG';
+  const up = 'Move: up';
+  const left = 'Move: left';
+  const down = 'Move: down';
+  const right = 'Move: right';
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
@@ -11,8 +16,14 @@ const connect = function () {
   conn.setEncoding('utf8');
   conn.on('connect', () => {
     console.log('Connected Successfully to Game');
-    conn.write('Name: Player1');
+    conn.write(`${name}`);
   });
+
+  setTimeout(() => { conn.write(`${up}`); }, 500);
+  setTimeout(() => { conn.write(`${down}`) }, 500);
+  setTimeout(() => { conn.write(`${left}`) }, 500);
+  setTimeout(() => { conn.write(`${right}`) }, 500);
+
   conn.on('data', (data) => {
     console.log('server says:', data);
   });
