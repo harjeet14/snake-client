844865b991af2b25cf7ff71eb3a3904e05829e8f
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-16 21:15:09 -0400

    require function

--- play.js
+++ play.js
@@ -1,17 +1,6 @@
-const net = require('net');
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log('server says:', data);
-  });
-  return conn;
-}
 
+//const net = require('net');
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
