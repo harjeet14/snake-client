eda8751c753440a5f826efea611030900b1ae5a3
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-17 14:16:01 -0400

    making connection to server

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 
+const { Server } = require('http');
 const net = require('net');
 
 const connect = function () {
@@ -8,9 +9,14 @@ const connect = function () {
   });
 
   conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log('Connected Successfully to Game');
+    conn.write('Name: Player1');
+  });
   conn.on('data', (data) => {
     console.log('server says:', data);
   });
+
   return conn;
 };
 
